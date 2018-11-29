4596abf45af3d687117c1df05a71d665fa490db1
Amare Abebe <amareabebe@AWOKEs-MacBook-Pro.local>
2018-11-29 00:00:01 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
-var cardOne = "queen";
-var cardTwo = "qhearts";
-var cardThree = "king";
-var cardFour ="khearts";
-var userFlipped = "user";
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo)
 
-console.log("User flipped " + cardOne);
+
+console.log("User flipped queen");
+console.log("User flipped king")
+
+for (var i = 0 ; i <cardsInPlay.length; i++){
+if(cardsInPlay[0] === cardsInPlay){
+alert("you found a match!");
+}else{
+alert("Sorry, try again");
+}
+}
